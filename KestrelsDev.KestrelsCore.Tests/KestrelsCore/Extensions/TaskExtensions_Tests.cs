f1ab5c6ab840b4ebb26315355662b9e15004a134
AceOfKestrels <mail@kestrels.dev>
2026-04-01 15:46:43 +0200

    Add task extensions tests

---
+++ KestrelsDev.KestrelsCore.Tests/KestrelsCore/Extensions/TaskExtensions_Tests.cs
@@ -0,0 +1,16 @@
+﻿using KestrelsDev.KestrelsCore.Extensions;
+
+namespace KestrelsDev.KestrelsCore.Tests.KestrelsCore.Extensions;
+
+public class TaskExtensions_Tests
+{
+    [Test]
+    public async Task Map__AppliesTransformFuncToResult()
+    {
+        Task<string> task = Task.FromResult("test");
+
+        int result = await task.Map(s => 1);
+
+        await Assert.That(result).EqualTo(1);
+    }
+}
