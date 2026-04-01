44e55f69eb6c75a0f88e912564f2fb83bcfb9450
AceOfKestrels <mail@kestrels.dev>
2026-04-01 14:07:39 +0200

    Add DotEnv tests

--- KestrelsDev.KestrelsCore.Tests/KestrelsCore/Env/DotEnv_Tests.cs
+++ KestrelsDev.KestrelsCore.Tests/KestrelsCore/Env/DotEnv_Tests.cs
@@ -1,10 +1,82 @@
-﻿namespace KestrelsDev.KestrelsCore.Tests.KestrelsCore.Env;
+﻿using KestrelsDev.KestrelsCore.Env;
+
+namespace KestrelsDev.KestrelsCore.Tests.KestrelsCore.Env;
 
 public class DotEnv_Tests
 {
+    private string RunPrefix => $"Run_{Random.Shared.Next()}_";
+
+    private string GetEnv(string key) => Environment.GetEnvironmentVariable(key) ?? "";
+
     [Test]
-    public async Task DotEnv_HasTests()
+    [Arguments("key=value", "value")]
+    [Arguments("key=", "")]
+    [Arguments("key=#value", "")]
+    [Arguments("key=value#", "value")]
+    [Arguments("key=v#alue", "v")]
+    [Arguments("key=v#alue#", "v")]
+    [Arguments("key#=value", "")]
+    [Arguments("ke#y=value", "")]
+    [Arguments("", "")]
+    [Arguments("# This is a comment. Set ENVs via key=value", "")]
+    [Arguments("key=\"#value\"", "#value")]
+    [Arguments("key=\"\"", "")]
+    [Arguments("key=\"value\"", "value")]
+    [Arguments("key=\"\"value\"\"", "\"value\"")]
+    [Arguments("key=\"\"#value\"\"", "\"#value\"")]
+    public async Task Load__NotYetSet__ParsesAndSetsEnv(string line, string expectedValue)
     {
-        Assert.Fail("No tests defined");
+        string runPrefix = RunPrefix;
+        List<string> lines = [runPrefix + line];
+
+        DotEnv.Load(lines);
+
+        string actual = GetEnv(runPrefix + "key");
+
+        if (actual != expectedValue)
+            Assert.Fail($"Expected value \"{expectedValue}\" does not match actual value \"{actual}\"");
+    }
+
+    [Test]
+    public async Task Load__MultipleLines__ParsesAllLines()
+    {
+        string runPrefix = RunPrefix;
+        List<string> lines = [runPrefix + "key1=value1", runPrefix + "key2=value2", runPrefix + "key3=value3"];
+
+        DotEnv.Load(lines);
+
+        await Assert.That(GetEnv(runPrefix + "key1")).EqualTo("value1");
+        await Assert.That(GetEnv(runPrefix + "key2")).EqualTo("value2");
+        await Assert.That(GetEnv(runPrefix + "key3")).EqualTo("value3");
+    }
+
+    [Test]
+    public async Task Load__SomeAlreadySet_NoOverwrite__SetsNewValues_DoesNotOverwriteExisting()
+    {
+        string runPrefix = RunPrefix;
+        List<string> lines = [runPrefix + "key1=value1", runPrefix + "key2=value2", runPrefix + "key3=value3"];
+
+        Environment.SetEnvironmentVariable(runPrefix + "key1", "originalValue");
+
+        DotEnv.Load(lines, false);
+
+        await Assert.That(GetEnv(runPrefix + "key1")).EqualTo("originalValue");
+        await Assert.That(GetEnv(runPrefix + "key2")).EqualTo("value2");
+        await Assert.That(GetEnv(runPrefix + "key3")).EqualTo("value3");
+    }
+
+    [Test]
+    public async Task Load__SomeAlreadySet_YesOverwrite__SetsNewValues_OverwritesExisting()
+    {
+        string runPrefix = RunPrefix;
+        List<string> lines = [runPrefix + "key1=value1", runPrefix + "key2=value2", runPrefix + "key3=value3"];
+
+        Environment.SetEnvironmentVariable(runPrefix + "key1", "originalValue");
+
+        DotEnv.Load(lines, true);
+
+        await Assert.That(GetEnv(runPrefix + "key1")).EqualTo("value1");
+        await Assert.That(GetEnv(runPrefix + "key2")).EqualTo("value2");
+        await Assert.That(GetEnv(runPrefix + "key3")).EqualTo("value3");
     }
 }
--- KestrelsDev.KestrelsCore/Env/DotEnv.cs
+++ KestrelsDev.KestrelsCore/Env/DotEnv.cs
@@ -36,17 +36,14 @@ public static class DotEnv
         string key = match.Groups[1].Value.Trim();
         string value = match.Groups[2].Value.Trim();
 
-        if (Environment.GetEnvironmentVariable(key).IsNullOrWhiteSpace() && !overwriteExisting)
+        if (!Environment.GetEnvironmentVariable(key).IsNullOrWhiteSpace() && !overwriteExisting)
             return;
 
-        match = value.Match("\"(.+)\".*");
+        match = value.Match("\"(.*)\".*");
 
         value = !match.Success || match.Groups.Count < 2
             ? value.Split('#').FirstOrDefault("").Trim()
             : match.Groups[1].Value;
-
-        if (value.IsNullOrWhiteSpace())
-            return;
 
         Environment.SetEnvironmentVariable(key, value);
     }
